f4469e1f8dc1a12e5d8752e8a34dc16b68782b78
nyf16 <onurferik@gmail.com>
2021-01-18 19:58:45 +0300

    Create Register Models

---
+++ PlaningApp.Shared/Models/RegisterRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PlaningApp.Shared.Models
+{
+    public class RegisterRequest
+    {
+        [Required]
+        [StringLength(50)]
+        [EmailAddress]
+        [Display(Name ="E-Mail")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        [Display(Name = "İsim")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        [Display(Name = "Soyisim")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Şifre")]
+        public string Password { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Şifreyi Onayla")]
+        public string ConfirmPassword { get; set; }
+    }
+}
