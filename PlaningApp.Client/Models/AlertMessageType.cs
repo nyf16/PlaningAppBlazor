f50afea4fe09fd517fe8a64f79d65011729e0770
nyf16 <onurferik@gmail.com>
2021-01-19 23:30:42 +0300

    AlertMessage Shared Components

---
+++ PlaningApp.Client/Models/AlertMessageType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlaningApp.Client.Models
+{
+    public enum AlertMessageType
+    {
+        Error, Warning, Success
+    }
+}
