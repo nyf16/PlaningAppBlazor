027e438cfdbb98444d6e26c36b6d765559f12970
nyf16 <onurferik@gmail.com>
2021-01-19 17:36:19 +0300

    Register Request to API

--- PlaningApp.Client/Program.cs
+++ PlaningApp.Client/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using PlaningApp.Shared.Services;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
@@ -12,9 +13,15 @@ namespace PlaningApp.Client
 {
     public class Program
     {
+        private const string URL = "https://plannerappserver20200228091432.azurewebsites.net";
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.Services.AddScoped<AuthenticationService>(s =>
+            {
+                return new AuthenticationService(URL);
+            });
+
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
---
+++ PlaningApp.Shared/Services/AuthenticationService.cs
@@ -0,0 +1,27 @@
+﻿using AKSoftware.WebApi.Client;
+using PlaningApp.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaningApp.Shared.Services
+{
+    public class AuthenticationService
+    {
+        private readonly string _baseUrl;
+
+        ServiceClient client = new ServiceClient();
+
+        public AuthenticationService(string url)
+        {
+            _baseUrl = url;
+        }
+
+        public async Task<UserManagerResponse> RegisterUserAsync(RegisterRequest request)
+        {
+            var response = await client.PostAsync<UserManagerResponse>($"{_baseUrl}/api/auth/register", request);
+            return response.Result;
+        }
+    }
+}
