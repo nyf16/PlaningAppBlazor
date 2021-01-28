cfce5d4686339294f9d2fb5a08667120f83bdac6
nyf16 <onurferik@gmail.com>
2021-01-28 03:06:22 +0300

    Authenticate user in browser

---
+++ PlaningApp.Client/LocalAuthenticationStateProvider.cs
@@ -0,0 +1,45 @@
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
+using PlaningApp.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PlaningApp.Client
+{
+    public class LocalAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private readonly ILocalStorageService _storageService;
+
+        public LocalAuthenticationStateProvider(ILocalStorageService storageService)
+        {
+            _storageService = storageService;
+        }
+
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            if (await _storageService.ContainKeyAsync("User"))
+            {
+                var userInfo = await _storageService.GetItemAsync<LocalUserInfo>("User");
+
+                var claims = new[]
+                {
+                    new Claim("Email", userInfo.Email),
+                    new Claim("İsim",userInfo.FirstName),
+                    new Claim("Soyİsim",userInfo.FirstName),
+                    new Claim("AccessToken", userInfo.AccessToken),
+                    new Claim(ClaimTypes.NameIdentifier,userInfo.Id)
+                };
+
+                var identity = new ClaimsIdentity(claims, "Bearer Token");
+                var user = new ClaimsPrincipal(identity);
+
+                return new AuthenticationState(user);
+            }
+
+            return new AuthenticationState(new System.Security.Claims.ClaimsPrincipal());
+        }
+    }
+}
--- PlaningApp.Client/Program.cs
+++ PlaningApp.Client/Program.cs
@@ -1,4 +1,5 @@
 using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -24,6 +25,10 @@ namespace PlaningApp.Client
             });
 
             builder.Services.AddBlazoredLocalStorage();
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, LocalAuthenticationStateProvider>();
+
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
